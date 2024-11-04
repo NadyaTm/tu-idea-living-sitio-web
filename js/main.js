1835162b55c2f497d166355954a38b5281b7aebb
Nadya Tm <nadyatome@gmail.com>
2024-11-04 19:16:48 -0300

    Actualización doc JavaScript

--- js/main.js
+++ js/main.js
@@ -16,3 +16,29 @@ if(navClose){
       navMenu.classList.remove('show-menu')
    })
 }
+
+/* Slider Home y servicios Desktop */
+
+let currentIndex = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slides img').length;
+
+function showNextSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+setInterval(showNextSlide, 5000);
+
+
+/* Slider Home movil */
+let indexOf = 0;
+const homeSliderMovil = document.querySelector('.homeSliderMovil');
+const totalhomeSliderMovil = document.querySelectorAll('.homeSliderMovil img').length;
+
+function showNexthomeSliderMovil() {
+    indexOf = (indexOf + 1) % totalhomeSliderMovil;
+    homeSliderMovil.style.transform = `translateX(-${indexOf * 100}%)`;
+}
+
+setInterval(showNexthomeSliderMovil, 5000);
